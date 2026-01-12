17791f4c73c27a5b92d7b158cfc24e91a263dda9
Lizmary Chardon <lizmary0209@gmail.com>
2026-01-12 01:58:44 -0500

    Finalize frontend deployment config

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
-export const BASE_URL = "http://localhost:3001";
+const BASE_URL = 
+process.env.NODE_ENV === "production"
+? "https://api.lizmary-wtwr.shareroute.org"
+: "http://localhost:3001";
 
 export function handleServerResponse(res) {
   if (!res.ok) {
@@ -59,3 +62,5 @@ export function removeCardLike(id, token) {
     },
   }).then(handleServerResponse);
 }
+
+export { BASE_URL };
